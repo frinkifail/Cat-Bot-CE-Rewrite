76c31327d75abf5a405e476bc5dc5ca8055bfb5d
Frinkifail <76682714+frinkifail@users.noreply.github.com>
2023-09-27 20:51:06 +0700

    new testing achs

--- code_resources/current_achs.py
+++ code_resources/current_achs.py
@@ -3,5 +3,11 @@ from .achivements import Achivement, AchivementManager
 achivements = {
     "testing": AchivementManager.new(
         "exact", phrase="devtest", description='Say "devtest"'
-    )
+    ),
+    "testing episode 2": AchivementManager.new(
+        "includes", phrase="cat", description='Have a "cat" in your message.'
+    ),
+    "testing episode 3": AchivementManager.new(
+        "exact", phrase="this is a very hidden message", hidden=True
+    ),
 }
